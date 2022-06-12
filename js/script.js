4dd8b9a235270cfc3a9b745c63ec1538609d1ba5
Minh Pham <31968090+Garlie@users.noreply.github.com>
2022-06-12 14:11:57 -0700

    Added project section

--- js/script.js
+++ js/script.js
@@ -2,38 +2,39 @@ const toggleButton = document.querySelector(".hamburger-button");
 const html = document.querySelector("html");
 const sections = document.querySelectorAll("section");
 const nav = document.querySelector("nav");
-const navbarLi = document.querySelectorAll(".navbar-links li");
+const navbarLi = document.querySelectorAll("nav li");
+
 
 toggleButton.addEventListener("click", () => {
     nav.classList.toggle("active");
     html.classList.toggle("active");
 })
 
-document.querySelectorAll(".navbar-links").forEach(n => n.
+document.querySelectorAll("nav li").forEach(n => n.
     addEventListener("click", () => {
         nav.classList.remove("active");
-        html.classList.toggle("active");
+        html.classList.remove("active");
     }))
 
 const options = {
-    threshold: "0.6", // 60% of the section should be visible
+    threshold: "0.7" // 70% of the section should be visible
 };
 
-const oberver = new IntersectionObserver((entries) => {
-    entries.forEach((e) => {
-        if(e.isIntersecting) {
+const oberver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
             // Change navbar style on scroll to next section
-            if(e.target.id !== "landing"){
-                nav.classList.add("scrolled");
+            if(entry.target.id !== "banner"){
+                nav.classList.add("nav-scrolled");
             }else{
-                nav.classList.remove("scrolled");
+                nav.classList.remove("nav-scrolled");
             }
 
             // Section Indicator
             navbarLi.forEach(li => {
                 // remove active class from other
                 li.classList.remove("active");
-                if(e.target.id === li.classList.value) {
+                if(entry.target.id === li.classList.value) {
                     li.classList.add("active");
                 }
             })
@@ -41,7 +42,9 @@ const oberver = new IntersectionObserver((entries) => {
     });
 }, options);
 
-sections.forEach((section) => {
+
+sections.forEach(section => {
     oberver.observe(section);
 });
+
 
